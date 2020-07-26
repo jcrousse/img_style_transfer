ba94105a41b4105ef5717ae3638cf393841cf940
john <jcrousse@gmail.com>
2020-07-26 12:43:11 +0200

    tried out the tensorflow documentation example

---
+++ main.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+
+import numpy as np
+import PIL.Image
+import time
+import functools
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import tensorflow_hub as hub
+
+
+def tensor_to_image(tensor):
+    tensor = tensor*255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor)>3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return PIL.Image.fromarray(tensor)
+
+
+def load_img(path_to_img):
+    max_dim = 512
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+
+def imshow(image, title=None):
+    if len(image.shape) > 3:
+        image = tf.squeeze(image, axis=0)
+    plt.imshow(image)
+    if title:
+        plt.title(title)
+
+content_path = 'images/sources/EJ_woods1.png'
+
+style_path = 'images/styles/night_sky_2.jpg'
+
+content_image = load_img(content_path)
+style_image = load_img(style_path)
+
+plt.subplot(1, 2, 1)
+imshow(content_image, 'Content Image')
+
+plt.subplot(1, 2, 2)
+imshow(style_image, 'Style Image')
+
+plt.show()
+
+hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
+stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
+
+tensor_to_image(stylized_image).show()
+tensor_to_image(stylized_image).save('tmp.png')
